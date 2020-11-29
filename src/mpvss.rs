7711786be164bfa3f4ee46a7413cdf0e8c40dddb
MathxH Chen <brainfvck@foxmail.com>
2020-11-29 15:55:02 +0800

    Update mpvss::new test

--- src/mpvss.rs
+++ src/mpvss.rs
@@ -26,19 +26,21 @@ impl MPVSS {
             length: 2048,
         };
     }
+
+    //pub fn init(length: u32) -> Self {}
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_q_g_prime() {
+    fn test_new() {
+        use super::MPVSS;
         use num_bigint::BigUint;
         use num_primes::Verification;
-        use std::ops::*;
-        let q: BigUint = BigUint::parse_bytes(b"ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca18217c32905e462e36ce3be39e772c180e86039b2783a2ec07a28fb5c55df06f4c52c9de2bcbf6955817183995497cea956ae515d2261898fa051015728e5a8aacaa68ffffffffffffffff", 16).unwrap();
-        assert!(Verification::is_safe_prime(&q));
-        let g: BigUint = q.sub(BigUint::from(1u32)).div(BigUint::from(2u32));
-        assert!(Verification::is_prime(&g));
-        assert!(!Verification::is_safe_prime(&g));
+
+        let mpvss = MPVSS::new();
+        assert!(Verification::is_safe_prime(&mpvss.q));
+        assert!(Verification::is_prime(&mpvss.g));
+        assert!(!Verification::is_safe_prime(&mpvss.g));
     }
 }
