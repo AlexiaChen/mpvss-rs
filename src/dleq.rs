3fdfc1fb4f45db34ce2dbb90b81d5e97ff6e2019
MathxH Chen <brainfvck@foxmail.com>
2020-12-13 18:23:16 +0800

    Update DLEQ

---
+++ src/dleq.rs
@@ -0,0 +1,28 @@
+// Copyright 2020 The MPVSS Authors.
+//
+// Code is licensed under AGPL License, Version 3.0.
+
+use num_bigint::BigUint;
+use std::option::Option;
+
+/// Chaum and Pedersen Scheme
+/// to prove that log_g1(h1)= log_g2(h2), for generators g1,h1,g2,h2 ∈ Gq,   Gq is group of order q and q is prime
+/// We denote this protocol by DLEQ(g1,h1,g2,h2), and it consists of the following steps, where the prover knows α such that h1 = g1^α and h2 = g2^α:
+/// - 1. The prover sends a1 = g1^w and a2 = g2^w to the verifier, with w ∈ R Zq
+/// - 2. The verifier sends a random challenge c ∈ R Zq to the prover.
+/// - 3. The prover responds with r = w − αc (mod q).
+/// - 4. The verifier checks that a1 = (g1^r) * (h1^c) and a2 = (g2^r) * (h2^c).
+#[derive(Debug, Clone)]
+pub struct DLEQ {
+    pub g1: BigUint,
+    pub h1: BigUint,
+    pub g2: BigUint,
+    pub h2: BigUint,
+
+    pub w: BigUint,
+    pub q: BigUint,
+    pub alpha: BigUint,
+    pub c: Option<BigUint>,
+}
+
+impl DLEQ {}
