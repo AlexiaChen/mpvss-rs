f25a28186fdc022f3eea62ccb82399cc9bf9ee4c
MathxH Chen <brainfvck@foxmail.com>
2020-12-26 14:39:22 +0800

    Impl lagrange_coefficient

--- src/util.rs
+++ src/util.rs
@@ -39,6 +39,32 @@ impl Util {
             Some(result)
         }
     }
+
+    /// returned (numerator: BigInt, denominator: BigInt) represent as  numerator/denominator
+    ///
+    /// where λ_i= ∏ j≠i = j/(j−i) is a Lagrange coefficient.
+    /// 1 <= i <= threshold  0 <= j < threshold
+    pub fn lagrange_coefficient(i: &i32, values: &[i32]) -> (BigInt, BigInt) {
+        if !values.contains(i) {
+            return (BigInt::zero(), BigInt::one());
+        }
+
+        let mut numerator = BigInt::one();
+        let mut denominator = BigInt::one();
+
+        let vec_to = values.to_vec();
+        let max = vec_to.iter().max().unwrap();
+        for j in 1..=*max {
+            if j != *i && values.contains(&j) {
+                numerator = numerator * j;
+                denominator = denominator * (j - *i);
+                println!("j: {}", j);
+                println!("numberator: {}", numerator);
+                println!("denominator: {}", denominator);
+            }
+        }
+        return (numerator, denominator);
+    }
 }
 
 #[cfg(test)]
@@ -80,5 +106,29 @@ mod tests {
             }
             None => {}
         }
+    }
+
+    #[test]
+    fn test_lagrange_coefficient() {
+        use super::BigInt;
+        use super::Util;
+        use super::{One, Zero};
+
+        let i_array = [9, 1, 2, 3];
+        let values = [0, 1, 2, 3, 4, 5, 6];
+        let result = Util::lagrange_coefficient(&i_array[0], &values);
+        assert_eq!(result, (BigInt::zero(), BigInt::one()));
+
+        // 0..=6 j/(j-1) = (2/1) * (3/2) * (4/3) * (5/4) * (6/5) = 720 / 120
+        let result = Util::lagrange_coefficient(&i_array[1], &values);
+        assert_eq!(result, (BigInt::from(720), BigInt::from(120)));
+
+        // 0..=6 j/(j-2) =  (1/-1) * (3/1) * (4/2) * (5/3) * (6/4) = 360 / -24
+        let result = Util::lagrange_coefficient(&i_array[2], &values);
+        assert_eq!(result, (BigInt::from(360), BigInt::from(-24)));
+
+        // 0..=6 j/(j-3) =  (1/-2) * (2/-1) * (4/1) * (5/2) * (6/3) = 240 / 12
+        let result = Util::lagrange_coefficient(&i_array[3], &values);
+        assert_eq!(result, (BigInt::from(240), BigInt::from(12)));
     }
 }
